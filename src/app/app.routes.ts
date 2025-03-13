2a9cdd24b24ceb617a475ff7847700b3e3d73267
Christoff Lombard <christoff@mabureau.co.za>
2025-03-13 15:21:38 +0200

    before Angular Version update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout/main-layout.component';
+import { LoginComponent } from './features/auth/login/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent, // Wraps routed components
+    children: [
+      { path: '', component: LoginComponent }, // Default route
+      { path: 'login', component: LoginComponent },
+    ],
+  },
+];
---
+++ src/app/features/auth/login/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule, MatIconModule, MatInputModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
