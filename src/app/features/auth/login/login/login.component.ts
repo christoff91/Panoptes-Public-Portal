d96b97328fe4c03a0333184259d480f087fdfdfc
Christoff Lombard <christoff@mabureau.co.za>
2025-03-18 11:59:31 +0200

    feat: updated login page

--- src/app/features/auth/login/login/login.component.ts
+++ src/app/features/auth/login/login/login.component.ts
@@ -1,24 +1,54 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule,
+  FormControl,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormFieldModule, MatIconModule, MatInputModule, MatCardModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
-  usernameError: string = '';
-  passwordError: string = '';
-  isFormValid: boolean = false;
-  isHovered: boolean = false;
-  isPasswordVisible: boolean = false;
-  isMobile: boolean = false;
+  loginForm: FormGroup;
+  // loginForm = new FormControl('', [Validators.required, Validators.email]);
+  hidePassword = true;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    }
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
 }
