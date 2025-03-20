89e70231bfd9fdb86decac6888ea0a2f3153b536
Christoff Lombard <christoff@mabureau.co.za>
2025-03-20 10:36:03 +0200

    feat: updated navigation

--- src/app/layout/navigation/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation/navigation.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResponsiveService } from '../../../core/services/responsive.service';
+
+// Material Imports
 import { MatDivider } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-navigation',
-  imports: [MatDivider],
+  imports: [
+    MatDivider,
+    MatToolbarModule,
+    MatIcon,
+    MatSidenavModule,
+    MatListModule,
+  ],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   isMobile: boolean = false;
+
+  constructor(private responsiveService: ResponsiveService) {}
+
+  ngOnInit() {
+    this.responsiveService.isMobile$.subscribe((isMobile) => {
+      this.isMobile = isMobile;
+    });
+  }
 }
